5685bfc46297c41fd91b118634cfe4c0e4517d34
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2019-01-17 16:16:27 -0500

    started using the auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_one :cart
   has_many :cart_items, through: :cart
   has_many :items, through: :cart_items
+  validates :username, uniqueness: { case_sensitive: false }
 end
