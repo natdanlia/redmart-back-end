52ae6b6bf219907ed052a6ded2cc1f82d84bd752
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2019-03-17 17:19:00 -0400

    removed require bye bug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-require "byebug"
+
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
   before_action :set_user, only: [:show, :update, :destroy]
